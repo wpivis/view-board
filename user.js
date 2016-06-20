477eabcdf5b78a46ea0fb821729205a41268f2e0
Ivan Melnikov <ivanempire@gmail.com>
2016-06-20 12:01:02 -0400

    Hackiest way ever - working square floats for demo board

---
+++ user.js
@@ -0,0 +1,22 @@
+var svg = d3.select("#container").append("svg")
+			.attr("width",410)
+			.attr("height",410)
+			.style("border","1px solid black");
+
+for(var colNum=0;colNum<7;colNum++) {
+	for(var rowNum=0;rowNum<7;rowNum++) {
+		svg.append("rect").attr("width","50")
+							.attr("height","50")
+							.attr("x",colNum*50+colNum*10)	
+							.attr("y",rowNum*50+rowNum*10)
+							.attr("fill","green")
+							.on("mouseover",function() {
+								d3.select(this).transition().duration(200).attr("fill","red")
+								var dataPoint = [d3.select(this).attr("x")/60,d3.select(this).attr("y")/60];
+								console.log(dataPoint);
+							})
+							.on("mouseout",function() {
+								d3.select(this).transition().duration(200).attr("fill","green")
+							});
+	}
+}
