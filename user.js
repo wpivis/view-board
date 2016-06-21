9f3aafacbe884b5280d5e97ca8866f2852ab6ff1
Ivan Melnikov <ivanempire@gmail.com>
2016-06-21 10:40:45 -0400

    Addressibility added

--- user.js
+++ user.js
@@ -10,26 +10,31 @@ for(var colNum=0;colNum<7;colNum++) {
 							.attr("height","50")
 							.attr("x",colNum*50+colNum*10)	
 							.attr("y",rowNum*50+rowNum*10)
+							.attr("id",function(d,i) {
+								return (1+7*rowNum+colNum).toString();
+							})
 							.attr("fill","green")
 							.on("mousedown",function() {
-								var dataPoint = [d3.select(this).attr("x")/60,d3.select(this).attr("y")/60];
+								var dataPoint = [d3.select(this).attr("id"),d3.select(this).attr("x")/60,d3.select(this).attr("y")/60];
 								d3.select(this).classed("selected",true);
 								gridLights.push(dataPoint);
 							})
 							.on("mouseover",function() {
 								if(d3.select(this).classed("selected")!=true && gridLights.length!=0) {
 									d3.select(this).classed("selected",true);
-									var dataPoint = [d3.select(this).attr("x")/60,d3.select(this).attr("y")/60];
+									var dataPoint = [d3.select(this).attr("id"),d3.select(this).attr("x")/60,d3.select(this).attr("y")/60];
 									gridLights.push(dataPoint);
 								}
 							})
 							.on("mouseup",function() {
 								svg.selectAll("rect.selected").classed("selected",false);
-								sendToServer();
+								console.log(gridLights);
+								//sendToServer();
 								gridLights=[];
 							});
 	}
 }
+
 var sendToServer = function() {
 	$.post( "/userInput",gridLights).done(function(data) {
 		alert("Server said:"+data);
