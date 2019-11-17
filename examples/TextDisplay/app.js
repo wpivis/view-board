9b1adb5a64eee5a8d506eda30f4b7d7f0f50698e
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2019-11-17 15:49:26 -0500

    Added pixel font, example and  refactored errorhandling
    Implemented a small pixel font - around 3x6 to 5x6 in size. Based on
    https://robey.lag.net/2010/01/23/tiny-monospace-font.html
    
    I also refactored error handling, instead of console.log, errors are now
    thrown properly.

---
+++ examples/TextDisplay/app.js
@@ -0,0 +1,27 @@
+var Nopixel = require('../../node_modules/nopixel');
+var nopixel = new Nopixel("../../config.json");
+
+nopixel.on('ready', function() {
+	nopixel.clear();
+	var count = 9;
+	var countdown = undefined;
+	var write1 = undefined;
+
+	countdown = setInterval(function() {
+		nopixel.clear();
+		if(count == 0) {
+			clearInterval(countdown);
+			nopixel.clear();
+			write1 = nopixel.write("Welcome to the nopixel board!", "cyan", 100);
+			nopixel.update();
+		}
+		nopixel.writeLetter(count.toString(), "red", 2,1);
+		nopixel.update();
+		count--;
+	},1000);
+
+	setTimeout(function() {
+		clearInterval(write1);
+	}, 20000)
+
+})
