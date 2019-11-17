0dddd6455a79dfbf6cd4dee5adbbacdcfa866197
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2019-11-16 23:57:01 -0500

    Finished HelloWorld example
    Fixed a quick bug that stems from shoddy opcode decision. Added a hello
    world example that randomizes colors on the board and independently
    changes them.

--- examples/HelloWorld/hello_world.js
+++ examples/HelloWorld/hello_world.js
@@ -1,65 +1,88 @@
-var Nopixel = require('nopixel');
+var Nopixel = require('../../node_modules/nopixel');
 var tinyColor = require("tinycolor2");
 //Set the configuration
-var nopixel = new Nopixel("./config.json");
+var nopixel = new Nopixel("../../config.json");
 
+var FPS = 24;
+var BOARD_SIZE = 49;
+var Board = null;
+
+// Color object, which tracks its own color, color transition & progress.
 function Color(fps, duration) {
 	this.duration = duration; //Duration in millisec
 	this.fps = fps; 
-	this.targetColor = undefined;
-	this.color = undefined;
+	this.color = tinyColor.random().toHsv();
+	this.targetColor = tinyColor.random().toHsv();
 	this.pause = false;
-	this.init = function() {
-		this.color = tinyColor.random();
-		getNewTarget();
-	}
-
+	this.off = false;
+	this.totalStep = parseInt(this.duration / this.fps);
+	this.stepCount = 0;
 	this.getNewTarget = function() {
-		while(targetColor == color && targetColor) {
-			this.targetColor = tinyColor.random();
+		this.targetColor = tinyColor.random().toHsv();
+	}
+	this.export = function() {
+		if(this.off) {
+			this.pause = true;
+			return tinyColor("black").toHsvString();
 		}
+
+		return tinyColor(this.color).toHsvString();
 	}
 
-	//Transition to targetColor
-	this.tick = function() {
-		color.toRgb();
-		targetColor.toRgb();
-
-		if(pause) {
+	//Transition to this.targetColor
+	this.step = function() {
+		if(this.pause) {
 			return;
 		}
 
-		var distance = [];
+		var percent = this.stepCount / this.totalStep;
+		if(this.stepCount == this.totalStep) {
+			this.stepCount = 0;
+			percent = 0;
+			this.getNewTarget();
+		}
 
+		this.color.h = ((this.targetColor.h - this.color.h) * percent) + this.color.h;
+		this.color.s = ((this.targetColor.s - this.color.s) * percent) + this.color.s;
+		this.color.v = ((this.targetColor.v - this.color.v) * percent) + this.color.v;
+		this.stepCount++;
 	}
 }
 
-var color1 = new Color(60, 5000);
-
-console.log(color1);
-
-var i = 0;
 nopixel.on('ready', function() {
 	//Clear the board
 	nopixel.clear();
 	nopixel.update();
 	//Start everything
+	Board = new Array(BOARD_SIZE);
+
+	for(var i =0; i < BOARD_SIZE; i++) {
+		Board[i] = new Color(FPS, parseInt(Math.random() * (10000 - 500) + 500));
+		// Board[i] = new Color(FPS, 1000);
+	}
 
 	setInterval(function() {
-		console.log(i);
-
-		if(i == 49) {
-			i = 0;
-		}
-		nopixel.clear();
-		nopixel.update();	
-		nopixel.setPixel(i%7, parseInt(i/7), "red");
-		nopixel.update();
-		i++;
-	}, 50); 
+	 	Board.forEach(function(el, i, array) {
+	 		el.step();
+	 		nopixel.setPixel(i%7, parseInt(i/7), el.export());
+	 	})
+	 	nopixel.update();
+	}, 1000/FPS); 
 })
 	
+nopixel.on('pressed', function(eventDetail) {
+	if(Board[eventDetail.index].pause) {
+		Board[eventDetail.index].pause = false;
+	} else {
+		Board[eventDetail.index].pause = true;
+	}
+})
 
-nopixel.on('clicked', function() {
-	i = 0;
+nopixel.on('pressed', function(eventDetail) {
+	if(Board[eventDetail.index].off) {
+		Board[eventDetail.index].off = false;
+		Board[eventDetail.index].pause = false;
+	} else {
+		Board[eventDetail.index].off = true;
+	}
 })
